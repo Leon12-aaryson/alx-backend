d10ab39e7a3fe5308794035039c8fc86b2fb6ec1
Leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2024-04-10 12:51:27 +0300

    language and internationalizing files"

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''
+instantiate babel in the python app module
+'''
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.object('Config')
+
+
+@app.route('/')
+def index():
+    return render_template('1-index.html')
